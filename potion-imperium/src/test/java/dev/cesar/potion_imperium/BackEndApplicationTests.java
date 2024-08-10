d20eba4a8ea515bda9ea1cf31cc032f35e84e5cf
CeezR <cesarforthomme@gmail.com>
2024-08-10 11:12:56 +0200

    feat: backend initialization

---
+++ potion-imperium/src/test/java/dev/cesar/potion_imperium/BackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cesar.potion_imperium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
