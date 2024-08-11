fc9823710b84b09ea6156520bd9b2a61d7f533c5
CeezR <cesarforthomme@gmail.com>
2024-08-11 11:21:25 +0200

    feat: openAi Client and null promp method added

--- src/main/java/dev/cesar/potion_imperium/client/OpenAiClient.java
+++ src/main/java/dev/cesar/potion_imperium/client/OpenAiClient.java
@@ -1,2 +1,29 @@
-package dev.cesar.potion_imperium.client;public class OpenAiClient {
+package dev.cesar.potion_imperium.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+
+@Component
+public class OpenAiClient {
+
+    @Value("${openai.model}")
+    private String model;
+
+    @Value("${openai.api.url}")
+    private String apiUrl;
+
+    private final RestClient restClient;
+
+    @Autowired
+    public OpenAiClient(RestClient.Builder restClientBuilder) {
+        this.restClient = restClientBuilder.build();
+    }
+
+    public String promt() {
+        return restClient.post().uri(apiUrl).retrieve().body(String.class);
+    }
+
+
 }
